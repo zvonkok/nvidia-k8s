bfe582dd42d828c032d7fae8d850bc7e8ce6e80d
Zvonko Kosic <zkosic@redhat.com>
2018-01-17 11:27:03 +0000

    Damn! ... tabs

--- daemonset/nvidia-installer.sh
+++ daemonset/nvidia-installer.sh
@@ -1,4 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
+
+atomic pull --storage ostree docker.io/zvonkok/redhat:nvidia-384.81
+atomic install --system --system-package=no docker.io/zvonkok/redhat:nvidia-384.81
+
+# fix symlinks, something bogus going on
+ln -s /opt/nvidia/lib/libcuda.so.384.81 /opt/nvidia/lib/libcuda.so.1
+ln -s /opt/nvidia/lib/libnvidia-cfg.384.81 /opt/nvidia/lib/libnvidia-cfg.so.1
+ln -s /opt/nvidia/lib/libnvidia-ml.so.384.81 /opt/nvidia/lib/libnvidia-ml.so.1
+ln -s /opt/nvidia/lib/libnvidia-opencl.so.384.81 /opt/nvidia/lib/libnvidia-opencl.so.1
+ln -s /opt/nvidia/lib/libnvidia-ptxjitcompiler.so.384.81 /opt/nvidia/lib/libnvidia-ptxjitcompiler.so.1
+
+# copy extra files to host system
+cp /usr/libexec/oci/hooks.d/oci-nvidia-container-runtime /host/usr/libexec/oci/hooks.d/oci-nvidia-container-runtime
+cp /etc/dracut.conf.d/nvidia.conf /host/etc/dracut.conf.d/nvidia.conf
+cp /etc/ld.so.conf.d/nvidia.conf /host/etc/ld.so.conf.d/nvidia.conf
+cp /etc/modprobe.d/nouveau.conf /host/etc/modprobe.d/nouveau.conf
+cp /etc/profile.d/nvidia.sh /host/etc/profile.d/nvidia.sh
+
+. /etc/profile.d/nvidia.sh
+
+chroot /host ldconfig
+
+nvidia-smi
+
+
+
+
 
 
 
